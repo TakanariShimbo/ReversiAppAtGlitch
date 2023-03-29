d2493ed580a52f6b79e0871b078e88b9c32e2e74
TakanariShimbo <takanari.shimbo+github@gmail.com>
2023-03-30 06:56:21 +0900

    revise

--- model.py
+++ model.py
@@ -3,8 +3,6 @@ from stone import ReversiStone
 
 """
 ToDo
-コマをおける場所の提案
-パスの実施
 最後の点数計算
 """
 class ReversiModel:
@@ -52,15 +50,13 @@ class ReversiModel:
         2. 次のコマの種類を返す
         """
         # Early return
-        if self.__board[i_board] != ReversiStone.CANDIDATE:
+        if self.__can_flip(i_board):
             return False
         
-        # コマをひっくり返す
+        # コマを置き、ひっくり返す
         current_stone_kind = self.__turn
-        i_turns = self.__flip_list(i_board, current_stone_kind)
-        self.__board[i_board] = current_stone_kind
-        for i_turn in i_turns:
-            self.__board[i_turn] = current_stone_kind
+        self.__flip_stones(i_board, current_stone_kind)
+        
 
         # 次のコマを置ける候補を探す
         reverse_stone_kind = self.__get_reverse_stone_kind(self.__turn)
@@ -81,8 +77,70 @@ class ReversiModel:
             return True
         
         # どちらの色の候補も見つからなかったのでゲーム終了
+        self.__turn = ReversiStone.EMPTY
         return True
+
+
+    def __can_flip(self, i_board: int) -> bool:
+        """
+        盤面のi_boardにコマを置いたときひっくり返せるかどうか
+        ひっくり返せるならTrue、返せなければFalseを返す
+        """
+        # 候補以外なら空のリストを返す
+        if self.__board[i_board] != ReversiStone.CANDIDATE:
+            return False
+        
+        return True
+        
+                
+    def __flip_stones(self, i_board: int, put_stone_kind: ReversiStone) -> None:
+        """
+        盤面のi_boardにput_stone_kind(BLACK or WHITE)を置かれたとして、コマをひっくり返す
+        ひっくり返されたらTrue、返されなければFalseを返す
+        """
+
+        # 相手のコマ
+        enemy_stone_kind = self.__get_reverse_stone_kind(put_stone_kind)
+
+        # ひっくり返されるマスのリスト
+        i_turns = []
+
+        # 方向パラメータ
+        UP, DOWN, LEFT, RIGHT = -10, 10, -1, 1
+        RIGHT_UP, RIGHT_DOWN, LEFT_UP, LEFT_DOWN = RIGHT+UP, RIGHT+DOWN, LEFT+UP, LEFT+DOWN
+        
+        # 8方向を走査
+        for unit_vec in (RIGHT, RIGHT_UP, UP, LEFT_UP, LEFT, LEFT_DOWN, DOWN, RIGHT_DOWN):
+            # ひっくり返す候補
+            temp = []
+
+            # i_boardとの差
+            vec = unit_vec
+
+            # 敵のコマである限り、v方向に進み続け、ひっくり返す候補に加え続ける
+            while self.__board[i_board + vec] == enemy_stone_kind:
+                temp.append(i_board + vec)
+                vec += unit_vec
+            
+            # 敵のコマのあとが自分のコマなら候補を確定する
+            if self.__board[i_board + vec] == put_stone_kind:
+                i_turns += temp
+
+        # コマを置く、ひっくり返す
+        self.__board[i_board] = put_stone_kind
+        for i_turn in i_turns:
+            self.__board[i_turn] = put_stone_kind
     
+
+    def __get_reverse_stone_kind(self, stone_kind: ReversiStone) -> ReversiStone:
+        """
+        反対のコマの種類を取得
+        """
+        if stone_kind == ReversiStone.BLACK:
+            return ReversiStone.WHITE
+        else:
+            return ReversiStone.BLACK
+            
 
     def __search_candidate(self, next_stone_kind: ReversiStone) -> bool:
         """
@@ -96,73 +154,43 @@ class ReversiModel:
 
         # 次の候補を探す
         is_find_candidate = False
-        for i, stone_kind in enumerate(self.__board.copy()):
-            if self.__can_put(i, next_stone_kind):
+        for i, _ in enumerate(self.__board.copy()):
+            if self.__is_candidate(i, next_stone_kind):
                 is_find_candidate = True
                 self.__board[i] = ReversiStone.CANDIDATE
 
         return is_find_candidate
-
-
-    def __get_reverse_stone_kind(self, stone_kind: ReversiStone) -> ReversiStone:
-        """
-        反対のコマの種類を取得
-        """
-        if stone_kind == ReversiStone.BLACK:
-            return ReversiStone.WHITE
-        else:
-            return ReversiStone.BLACK
         
 
-    def __can_put(self, i_board: int, put_stone_kind: ReversiStone) -> bool:
+    def __is_candidate(self, i_board: int, put_stone_kind: ReversiStone) -> bool:
         """
         盤面のi_boardにput_stone_kind(BLACK or WHITE)のコマを置けるかどうか
         置けるならTrue、置けなければFalseを返す
         """
-        i_turns = self.__flip_list(i_board, put_stone_kind)
-        if i_turns:
-            return True
-        else:
+
+        # 空コマ以外なら置けないことが確定
+        if self.__board[i_board] != ReversiStone.EMPTY:
             return False
 
-    
-    def __flip_list(self, i_board: int, put_stone_kind: ReversiStone):
-        """
-        盤面のi_boardにput_stone_kind(BLACK or WHITE)を置いたときに、
-        ひっくり返されるマスのリストを返す
-        """
-        # 候補以外なら空のリストを返す
-        if self.__board[i_board] not in [ReversiStone.EMPTY, ReversiStone.CANDIDATE]:
-            return []
-
         # 相手のコマ
-        if put_stone_kind == ReversiStone.WHITE:
-            enemy_stone_kind = ReversiStone.BLACK
-        else:
-            enemy_stone_kind = ReversiStone.WHITE
-
-        # ひっくり返されるマスのリスト
-        i_turns = []
+        enemy_stone_kind = self.__get_reverse_stone_kind(put_stone_kind)
 
         # 方向パラメータ
         UP, DOWN, LEFT, RIGHT = -10, 10, -1, 1
         RIGHT_UP, RIGHT_DOWN, LEFT_UP, LEFT_DOWN = RIGHT+UP, RIGHT+DOWN, LEFT+UP, LEFT+DOWN
         
         # 8方向を走査
-        for v in (RIGHT, RIGHT_UP, UP, LEFT_UP, LEFT, LEFT_DOWN, DOWN, RIGHT_DOWN):
-            # ひっくり返す候補
-            temp = []
-
+        for unit_vec in (RIGHT, RIGHT_UP, UP, LEFT_UP, LEFT, LEFT_DOWN, DOWN, RIGHT_DOWN):
             # i_boardとの差
-            delta = v
+            vec = unit_vec
 
-            # 敵のコマである限り、ひっくり返す候補に加え続ける
-            while self.__board[i_board + delta] == enemy_stone_kind:
-                temp.append(i_board + delta)
-                delta += v
+            # 敵のコマである限り、v方向に進み続ける
+            while self.__board[i_board + vec] == enemy_stone_kind:
+                vec += unit_vec
             
-            # 敵のコマのあとが自分のコマなら候補を確定する
-            if self.__board[i_board + delta] == put_stone_kind:
-                i_turns += temp
-
-        return i_turns
+            # 敵のコマのあとが自分のコマなら置けることが確定
+            if self.__board[i_board + vec] == put_stone_kind:
+                return True
+            
+        # 全方向探索してもひっくり返せるコマが見つからなければ置けないことが確定
+        return False
